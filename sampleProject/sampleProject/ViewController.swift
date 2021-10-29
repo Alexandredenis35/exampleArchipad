38164f8e67f363f97eb61238d11e16c959ba6173
DENIS <alexandre.denis@niji.fr>
2021-10-30 00:03:18 +0200

    add iOS project

---
+++ sampleProject/sampleProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sampleProject
+//
+//  Created by Alexandre DENIS on 30/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
